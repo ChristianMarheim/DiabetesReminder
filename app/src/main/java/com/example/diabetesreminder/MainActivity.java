d5ffcc5bdbe57ba9443e7d8e7a0441eea156bc4c
Admin <test>
2020-07-18 17:10:53 +0200

    TODO/Commit message - 18.07.2020:     It still works but there is something fucked up with doBusinessLogic().     It seems it was written for initial loading and not loading for old settings.     Should rewrite it to something simple, or just support all the cases.     The 4 permutations of
        what-alarm-is-for * what-day-is-the alarm
    
        needs to be represented everywhere for it to correctly load both features after restart.
        This means when it is restarted 3 days later after alarm was initially set,
        it should not set it 4 days forward again, but only set it 1 day forward. <- This is basically acceptance criteria
        Maybe I should play around with some unit tests for this? A good exercise in refactoring!
        Refactoring is probably gonna go to hell because I don't have any tests now so probably break
        it fast to try out and just revert.

--- app/src/main/java/com/example/diabetesreminder/MainActivity.java
+++ app/src/main/java/com/example/diabetesreminder/MainActivity.java
@@ -15,38 +15,59 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static android.content.SharedPreferences.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String KEY_LONG_TIMESTAMP_SPROYTE = "longtimestampsproyte";
-    private final String KEY_LONG_TIMESTAMP_SENSOR = "longtimestampsensor";
-
-    String CHANNEL_ID = "note";
-    String CHANNEL_ID_DEBUG = "noteDEBUG";
-
-
+    private static final boolean DEBUG = false;
 
     int ID = 001;
-    final long timePassedBeforeNotificationWarning = 1 * 1000 * 20; //final long threeDays = 3 * 1000 * 3600 * 24;
-    final long timePassedBeforeNotificationDeadline = 1 * 1000 * 30;    //final long fourDays = 4 * 1000 * 3600 * 24;
+    private final String APP_ID = "DIABETES_REMINDER_BETA_APP_ID";
+    private final String SENSOR_TIMESTAMP_KEY = "SENSOR_TIMESTAMP";
+    private final String SPROYTE_TIMESTAMP_KEY = "SPROYTE_TIMESTAMP";
+
+    String CHANNEL_ID = "NOTE_NOTIFICATION_CHANNEL";
+    String CHANNEL_ID_DEBUG = "DEBUG_NOTIFICATION_CHANNEL";
+
+    final long timePassedBeforeNotificationWarning = 1 * 1000 * 10; //final long threeDays = 3 * 1000 * 3600 * 24;
+    final long timePassedBeforeNotificationDeadline = 1 * 1000 * 15;    //final long fourDays = 4 * 1000 * 3600 * 24;
 
     long timePassedSproyte = 0;
     long timePassedSensor = 0;
     long longTimestampSensor;
     long longTimestampSproyte;
 
-    SharedPreferences sensorPreferences;
-    SharedPreferences sproytePreferences;
+    SharedPreferences preferences;
 
     boolean init = false;
+
+    /*
+    TODO/Commit message - 18.07.2020:
+    It still works but there is something fucked up with doBusinessLogic().
+    It seems it was written for initial loading and not loading for old settings.
+    Should rewrite it to something simple, or just support all the cases.
+    The 4 permutations of
+
+    what-alarm-is-for * what-day-is-the alarm
+
+    needs to be represented everywhere for it to correctly load both features after restart.
+    This means when it is restarted 3 days later after alarm was initially set,
+    it should not set it 4 days forward again, but only set it 1 day forward. <- This is basically acceptance criteria
+    Maybe I should play around with some unit tests for this? A good exercise in refactoring!
+    Refactoring is probably gonna go to hell because I don't have any tests now so probably break
+    it fast to try out and just revert.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorPreferences = MainActivity.this.getSharedPreferences(KEY_LONG_TIMESTAMP_SENSOR, Context.MODE_PRIVATE);
-        sproytePreferences = MainActivity.this.getSharedPreferences(KEY_LONG_TIMESTAMP_SPROYTE, Context.MODE_PRIVATE);
+        preferences = MainActivity.this.getSharedPreferences(APP_ID, Context.MODE_PRIVATE);
+        SharedPreferences preferences2 = MainActivity.this.getSharedPreferences(APP_ID, Context.MODE_PRIVATE);
+        SharedPreferences preferences3 = MainActivity.this.getSharedPreferences(APP_ID, Context.MODE_PRIVATE);
 
         //doBusinessLogic();
     }
@@ -54,20 +75,20 @@ public class MainActivity extends AppCompatActivity {
     private void doBusinessLogic() {
         updateDebugInfo("Before");
 
-        if(sensorPreferences.getLong(KEY_LONG_TIMESTAMP_SENSOR, 0) != 0){
+        if(preferences.getLong(SENSOR_TIMESTAMP_KEY, 0) != 0){
             timePassedSensor = getTimePassedSensor();
 
             if(timePassedSensor > timePassedBeforeNotificationWarning){
-                save(KEY_LONG_TIMESTAMP_SENSOR, new Date().getTime());
+                save(SENSOR_TIMESTAMP_KEY, new Date().getTime()); // FIXME Why the fuck save now? Put in a check if we have data instead?
                 displayNotificationSensor(null);
             }
         }
 
-        if(sproytePreferences.getLong(KEY_LONG_TIMESTAMP_SPROYTE, 0) != 0){
+        if(preferences.getLong(SPROYTE_TIMESTAMP_KEY, 0) != 0){
             timePassedSproyte = getSproyteTimePassed();
 
             if(timePassedSproyte > timePassedBeforeNotificationWarning){
-                save(KEY_LONG_TIMESTAMP_SPROYTE, new Date().getTime());
+                save(SPROYTE_TIMESTAMP_KEY, new Date().getTime());  // FIXME Why the fuck save now?
                 displayNotificationSproyte(null);
             }
         }
@@ -75,13 +96,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private long getTimePassedSensor() {
-        longTimestampSensor = sensorPreferences.getLong(KEY_LONG_TIMESTAMP_SENSOR, 0);
-
+        longTimestampSensor = preferences.getLong(SENSOR_TIMESTAMP_KEY, 0);
         return new Date().getTime() - longTimestampSensor;
     }
 
     private long getSproyteTimePassed() {
-        longTimestampSproyte = sproytePreferences.getLong(KEY_LONG_TIMESTAMP_SPROYTE, 0);
+        longTimestampSproyte = preferences.getLong(SENSOR_TIMESTAMP_KEY, 0);
         return new Date().getTime() - longTimestampSproyte;
     }
 
@@ -90,36 +110,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateDebugInfo(String caller) {
-        TextView infoText = (TextView) findViewById(R.id.infoText);
-        infoText.setText(getInfoText());
-        sendNote(caller + " Current state:" + getInfoText(), true);
-
-    }
-
-    private void updateDebugInfo() {
-        updateDebugInfo("");
+        if(DEBUG){
+            TextView infoText = (TextView) findViewById(R.id.infoText);
+            infoText.setText(getInfoText());
+            sendNote(caller + " Current state:" + getInfoText(), true);
+        }
     }
 
     private String getInfoText() {
-        return "longtimestampsensor - " + longTimestampSensor + "\n" +
-                "longtimestampsproyte - " + longTimestampSproyte + "\n" +
-                "timeSinceSproyteAlarmActiviated - " + timePassedSproyte + "\n" +
-                "timeSinceSensorAlarmActiviated - " + timePassedSensor + "\n" +
-                "timePassedBeforeNotificationWarning - " + timePassedBeforeNotificationWarning + "\n" +
-                "timePassedBeforeNotificationDeadline - " + timePassedBeforeNotificationDeadline;
+        if(!DEBUG)
+            return "";
+        return "\n\nDEBUG:\nSENSOR_TIMESTAMP_KEY=" + preferences.getLong(SENSOR_TIMESTAMP_KEY, 0) + "\n" +
+               "SPROYTE_TIMESTAMP_KEY=" + preferences.getLong(SPROYTE_TIMESTAMP_KEY, 0) + "\n" +
+               "new Date()" + new SimpleDateFormat().format(new Date().getTime()) + "\n" +
+               "timePassedSproyte=" + timePassedSproyte + "\n" +
+               "timePassedSensor=" + timePassedSensor + "\n" +
+                "longTimestampSensor=" + longTimestampSensor + "\n" +
+                "longTimestampSproyte=" + longTimestampSproyte + "\n" +
+                "timePassedBeforeNotificationWarning=" + timePassedBeforeNotificationWarning + "\n" +
+                "timePassedBeforeNotificationDeadline=" + timePassedBeforeNotificationDeadline + "\n";
     }
 
     private void save(String key, long value) {
-        SharedPreferences sharedPref = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
+        Editor editor = preferences.edit();
         editor.putLong(key, value);
         editor.commit();
-        sendNote("Saved the following preferences: " + getInfoText(), true);
+        if(DEBUG){
+            sendNote("Saved the following preferences: " + getInfoText(), true);
+        }
     }
 
     public void displayNotificationSproyte(View view) {
         // SLEEP 5 SECONDS HERE ...
         Handler handler = new Handler();
+        save(SPROYTE_TIMESTAMP_KEY, new Date().getTime());
 
         handler.postDelayed(new Runnable() {
             public void run() {
@@ -137,6 +161,7 @@ public class MainActivity extends AppCompatActivity {
     public void displayNotificationSensor(View view) {
         // SLEEP 5 SECONDS HERE ...
         Handler handler = new Handler();
+        save(SENSOR_TIMESTAMP_KEY, new Date().getTime());
 
         handler.postDelayed(new Runnable() {
             public void run() {
@@ -156,18 +181,18 @@ public class MainActivity extends AppCompatActivity {
         sendNote(text, false);
     }
 
-    private void sendNote(String text, boolean debug) {
+    private void sendNote(String text, boolean debugNotificationChannel_NOT_DEBUG_MODE_MIND_YOU) {
         if (!init)
             createNotificationChannel();
 
         if(text.contains("Sproyte"))
-            timePassedSproyte = 0;
+            timePassedSproyte = 0; //FIXME Må fikse dagen-foer-warning vs bytte-idag-warning
 
         if(text.contains("Sensor"))
-            timePassedSensor = 0;
+            timePassedSensor = 0;  //FIXME Må fikse dagen-foer-warning vs bytte-idag-warning
 
         NotificationCompat.Builder builder;
-        if(debug){
+        if(debugNotificationChannel_NOT_DEBUG_MODE_MIND_YOU){
             builder = new NotificationCompat.Builder(this, CHANNEL_ID_DEBUG);
         } else {
             builder = new NotificationCompat.Builder(this, CHANNEL_ID);
